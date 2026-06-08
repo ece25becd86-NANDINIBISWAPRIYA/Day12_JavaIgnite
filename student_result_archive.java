791b3e356d316c038c40a31a21ff6cca729011d6
rite2harini <rite2harini@gmail.com>
2026-06-08 12:49:14 +0530

    Create student_result_archive.java

---
+++ student_result_archive.java
@@ -0,0 +1,37 @@
+/*
+Student Result Archive
+
+Create:
+
+Student
+│
+| - GraduateStudent
+
+Requirements:
+
+Override:
+
+calculateGrade()
+
+for GraduateStudent.
+
+Overloading:
+
+Create:
+
+displayResult()
+
+displayResult(String semester)
+
+Use:
+
+FileOutputStream
+
+to save student result data.
+
+Use:
+
+FileInputStream
+
+to read and display the data.
+  */
